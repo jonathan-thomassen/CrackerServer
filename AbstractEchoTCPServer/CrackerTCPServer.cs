e5b8df9c46ea6cb4a3ef1737da380cf18fffd9ad
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2022-03-03 09:57:12 +0100

    Chunks

--- AbstractEchoTCPServer/CrackerTCPServer.cs
+++ AbstractEchoTCPServer/CrackerTCPServer.cs
@@ -3,7 +3,7 @@ using System.Collections.Concurrent;
 
 namespace AbstractEchoTCPServer {
     public class CrackerTCPServer : AbstractTCPServer {
-        private int _chunkSize = 100000;
+        private int _chunkSize = 5000;
         private int _nextClientStartpoint = 0;
 
         public CrackerTCPServer(int port, string name, int shutDownPort, string debugLevel) : base(port, name, shutDownPort, debugLevel) {
@@ -12,22 +12,27 @@ namespace AbstractEchoTCPServer {
 
         public override void TcpServerWork(StreamReader reader, StreamWriter writer) {
             List<string> fullDictionary = new List<string>();
+            BlockingCollection<List<string>> chunks = new BlockingCollection<List<string>>();
 
             using (FileStream fs = new FileStream("webster-dictionary.txt", FileMode.Open, FileAccess.Read))
 
             using (StreamReader dictionary = new StreamReader(fs)) {
+                var n = 0;
                 while (!dictionary.EndOfStream) {
-                    string dictionaryEntry = dictionary.ReadLine();                    
-                    fullDictionary.Add(dictionaryEntry);                    
+                    if (n % _chunkSize == 0) {
+                        chunks.Add(new List<string>());
+                    }
+                    string dictionaryEntry = dictionary.ReadLine();
+                    chunks.Last().Add(dictionaryEntry);
+                    n++;
                 }
-            }           
-
+            }
 
 
             string line = reader.ReadLine();
-            reader.re
+            //reader.re
 
-            writer.WriteAsync()
+            //writer.WriteAsync()
             writer.WriteLine(line);
             _nextClientStartpoint += _chunkSize;
             writer.Flush();
